be8442bb29cce7491c5a5c1409fbc36a2dc86d5d
bgzjw <58939119+bgzjw@users.noreply.github.com>
2019-12-21 15:01:03 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #=================================================
 # Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+Lisence: MIT
+Author: ZJW@Openwrt
+GitHub Channel: https://github.com/bgzjw 
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.0.1/192.168.0.8/g' openwrt/package/base-files/files/bin/config_generate
